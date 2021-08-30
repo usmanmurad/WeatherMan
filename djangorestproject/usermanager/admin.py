5b5fe4aaa1150609b29cffd51f78a5a9b55ec146
usmanmurad <usmanmurad@A006-00590.local>
2021-08-30 18:40:00 +0500

    complete django rest api application with token based authentication

--- djangorestproject/usermanager/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from usermanager.models import User
-
-admin.site.register(User)
--- djangorestproject/usermanager/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    bio = models.CharField(max_length=255, blank=True)
-    phone = models.CharField(max_length=12, blank=True)
